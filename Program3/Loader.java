57c781fc909a7e376d3cb6d2c885c18849dd0103
SirKlawj <hirounm@gmail.com>
2026-03-29 17:01:51 -0700

    does this convert to lf?

--- Program3/Loader.java
+++ Program3/Loader.java
@@ -13,15 +13,24 @@ The demo, in a try/catch does
 
 Instead of sending a query, I need to send insert statements
 (dont forget to turn off autocommit first)
+
+supply the usernaem, password, and 
 */
 
 import java.io.*;
 import java.sql.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 public class Loader {
     final static String oracleURL = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
 
     public static void main(String[] args) {
+        //I guess first you should form the query
+        String query = formQuery(args[2]);
         Connection dbConnection = connectToDB(args);
         //All done. Close the connection
         try { dbConnection.close(); } catch(SQLException e) {
@@ -30,9 +39,32 @@ public class Loader {
         }
     }
 
+    private static String formQuery(String filename) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filename));
+        } catch(FileNotFoundException e) {
+            System.err.println("Unable to open the csv file");
+            System.exit(-1);
+        }
+        //Read each line of the file, get values, and insert them into the table
+        try {
+            String currentLine = null;
+            while((currentLine = br.readLine()) != null) {
+                System.out.println(currentLine);
+            }
+            //close when all done
+            br.close();
+        } catch(IOException e) {
+            System.err.println("Error reading from the csv file");
+            System.exit(-1);
+        }
+        return "";
+    }
+
     private static Connection connectToDB(String[] args) {
         String username = null, password = null;
-        if(args.length == 2) {
+        if(args.length >= 2) {
             username = args[0]; password = args[1];
         } else {
             System.out.println("Please supply just a username then your password");
