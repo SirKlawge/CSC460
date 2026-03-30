1f3eaa0a88e49fc37df320aa8491610974552ebf
SirKlawj <hirounm@gmail.com>
2026-03-29 22:04:44 -0700

    change from fucking garbage utf16 to 8

--- Program3/Loader.java
+++ Program3/Loader.java
@@ -14,8 +14,9 @@ The demo, in a try/catch does
 Instead of sending a query, I need to send insert statements
 (dont forget to turn off autocommit first)
 
-supply the usernaem, password, and 
+supply the usernaem, password, csv file name, and last 2 digits of the year
 Adding one last littel bit
+
 */
 
 import java.io.*;
@@ -24,26 +25,31 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class Loader {
     final static String oracleURL = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
 
     public static void main(String[] args) {
         //I guess first you should form the query
-        String query = formQuery(args[2]);
-        Connection dbConnection = connectToDB(args);
+        String query = formQuery(args);
         //All done. Close the connection
-        try { dbConnection.close(); } catch(SQLException e) {
-            System.out.println("Error closing the connection");
-            System.exit(-1);
-        }
     }
 
-    private static String formQuery(String filename) {
+    private static String formQuery(String[] args) {
+        String filename = args[2], year = args[3];
         BufferedReader br = null;
+        BufferedWriter bw = null;
         try {
-            br = new BufferedReader(new FileReader(filename));
+            bw = new BufferedWriter(new FileWriter("output.txt", StandardCharsets.UTF_8));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(filename), StandardCharsets.UTF_8));
         } catch(FileNotFoundException e) {
             System.err.println("Unable to open the csv file");
             System.exit(-1);
@@ -52,9 +58,24 @@ public class Loader {
         try {
             String currentLine = null;
             while((currentLine = br.readLine()) != null) {
-                System.out.println(currentLine);
+                String[] tupleArray = currentLine.split(",");
+                String insertString = "insert into vabram.RailIncident" + year + " values (";
+                for(int i = 0; i < tupleArray.length -1; i++) { //Dont forget to add last value, an int
+                    /*
+                    i = 3 are dates
+                    7, 10, 11 are integer fields
+                    the rest are strings
+                    */
+                    if(i == 3) insertString += "TO_DATE('" + tupleArray[i] + "', 'YYYY-MM-DD'), ";
+                    if(i != 3 && i != 7 && i != 10 && i != 11) insertString += "'" + tupleArray[i] + "',";
+                    if(i == 7 || i == 10) insertString += tupleArray[i] + ", ";
+                }
+                insertString += tupleArray[11] + ");";
+                bw.write(insertString);
+                bw.newLine();
             }
             //close when all done
+            bw.close();
             br.close();
         } catch(IOException e) {
             System.err.println("Error reading from the csv file");
